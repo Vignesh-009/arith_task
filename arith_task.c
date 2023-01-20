d0f0c201469f606a087e8e6f16bd17dea28e3471
arunch96 <aravikumar@msystechnologies.com>
2023-01-20 14:34:14 +0530

    arith_task1

--- arith_task.c
+++ arith_task.c
@@ -5,5 +5,9 @@ int main(){
         int b=20;
         int c=a+b;
         printf("Addition of %d + %d =%d \n",a,b,c);
+        
+        //sub:
+        int c=a-b;
+        printf("Subtraction of %d - %d =%d \n",a,b,c);
 	return 0;
 }
